bfc4574b195888c301d8dcdc036e08b2a7b987a3
Rohit Singh <31705884+rohitlb@users.noreply.github.com>
2017-09-30 11:05:55 +0530

    Drug registration added
    its DB management is done, frontend part is still pending

--- model/drugindex.js
+++ model/drugindex.js
@@ -1,16 +1,20 @@
 var mongoose = require('mongoose');
 
-var DrugIndex = new mongoose.Schema({
-    Company : [{
-                company_name : {type:String},
-                brand : [{
-                            brand_name : {type:String},
-                            salt : {type:String},
-                            strength : {type:String},
-                            packaging : {type:String},
-                            price : {type:String}
-                        }]
-              }]
+
+var Medicines = new mongoose.Schema({
+    Company: [{
+        Company_name: {type: String},
+        Brands: [{
+            Brand_name: {type: String},
+            Salts: [{
+                Salt_name: {type: String},
+                Strength: {type: String}
+            }],
+            Dosage_form: {type: String},
+            Price: {type: String}
+        }]
+    }]
 });
 
-module.exports = mongoose.model('DrugIndex',DrugIndex);
+
+module.exports=mongoose.model('medicines',Medicines);
