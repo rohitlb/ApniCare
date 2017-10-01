bfe3afd73e3bfe25f6b745bf29afc8e823b3ee51
Sonibrothers <32388884+Sonibrothers@users.noreply.github.com>
2017-10-01 16:12:14 +0530

    dropdown css added

--- public/js/test.js
+++ public/js/test.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){
 
+    $('.dropdown-button').dropdown({
+        inDuration: 300,
+        outDuration: 225,
+        constraintWidth: true,
+        hover: true,
+        gutter: 0,
+        belowOrigin: true,
+        alignment: 'left',
+        stopPropagation: false
+    });
+
+
     $('.carousel').carousel();
     $('.carousel').carousel('next');
     $('.carousel').carousel('next',3);
