1b13d0046269dae498250c23f79080b1c3a5bf0d
Rohit Singh <31705884+rohitlb@users.noreply.github.com>
2017-10-02 10:32:54 +0530

    hiding x powered enabled

--- app.js
+++ app.js
@@ -14,8 +14,9 @@ var Drug = require('./model/drugindex');
 
 //declare the app
 var app = express();
+
 // to hide X-Powered-By for Security,Save Bandwidth in ExpressJS(node.js)
-//app.disable('x-powered-by');
+app.disable('x-powered-by');
 
 //configure the app
 app.set('port',4000);
