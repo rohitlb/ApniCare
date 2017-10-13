a3504bdf40aa386400853b2faf08d7760ea43e11
Rohit Singh <31705884+rohitlb@users.noreply.github.com>
2017-10-13 20:09:02 +0530

    database changed to ApniCare

--- app.js
+++ app.js
@@ -185,7 +185,7 @@ app.post('/login',function (req,res) {
     });
 
 //data base connection and opening port
-    var db = 'mongodb://localhost/Works';
+    var db = 'mongodb://localhost/ApniCare';
     mongoose.connect(db, {useMongoClient: true});
 
 //connecting database and starting server
