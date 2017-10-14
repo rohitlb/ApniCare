deb527cac9a430bbb28c937fd2769623d13565b3
Rupesh <rupeshyadav94.ry@gmail.com>
2017-10-14 10:25:43 +0530

    session with id

--- app.js
+++ app.js
@@ -153,7 +153,7 @@ app.post('/login',function (req,res) {
             console.log(result);
             if(result) {
                         console.log("Successfully login");
-                        req.session.userID = req.body.number;
+                        req.session.userID = result._id;
                         if (req.session.userID) {
                             res.send({status: "success", message: "successfully login" ,number: req.session.userID});
                             res.end();
