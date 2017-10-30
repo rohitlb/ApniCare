0a65a32b4dd67ce5f0554036387f57b9b9f6d4ba
nidhi <32388884+Sonibrothers@users.noreply.github.com>
2017-10-31 01:45:19 +0530

    forgotpassword

--- public/js/test.js
+++ public/js/test.js
@@ -52,16 +52,16 @@ $(document).ready(function() {
                     }
                     else {
                         Materialize.toast(result.message, 2000);
-<<<<<<< HEAD
+
                             //$('#number').removeAttr('disabled');
                             //$('#password').attr('disabled','disabled');
 
 
-=======
+
 
                             $('#number').removeAttr('disabled');
                             $('#password').attr('disabled','disabled');
->>>>>>> 716d1db4ee5e5f6639cff4bd6ac4e612d2ccde5d
+
                     }
 
                 },
@@ -192,7 +192,7 @@ $(document).ready(function() {
         )
     });
     //for forgot password
-    $('#resetOTP').click(function () {
+    $('#OTPforForgot').click(function () {
 
 
         // var name = $('#name').val();
@@ -205,7 +205,7 @@ $(document).ready(function() {
 
         $.ajax(
             {
-                url: "/forgotpassword",
+                url: "/checkforgotpassword",
                 method: 'POST',
                 data: JSON.stringify(data),
                 contentType: 'application/json',
@@ -219,11 +219,46 @@ $(document).ready(function() {
                     }
                     else {
                         Materialize.toast(result.message, 2000);
-                        $('#change').click(function () {
-                            $('#number').removeAttr('disabled');
-                            $('#password').attr('disabled','disabled');
+                        //$('#change').click(function () {
+                          //  $('#number').removeAttr('disabled');
+                            //$('#password').attr('disabled','disabled');
 
-                        });
+                    //});
+                    }
+
+                },
+                error: function (err) {
+
+                }
+            }
+
+        )
+
+    });
+    //for verification
+    $('#verify1').click(function () {
+        var otp = $('#otp1').val();
+
+        var data = {
+            number: otp
+        };
+
+        $.ajax(
+            {
+                url: "/VerifyOTP",
+                method: 'POST',
+                data: JSON.stringify(data),
+                contentType: 'application/json',
+                success: function (result) {
+
+                    if (result) {
+                        Materialize.toast(result.message, 2000);
+                        $('#password').removeAttr('disabled');
+
+                    }
+                    else {
+                        Materialize.toast(result.message, 2000);
+
                     }
 
                 },
@@ -236,22 +271,24 @@ $(document).ready(function() {
 
     });
 
+
+
     $('#againLogin').click(function () {
 
         //$('#preloader').show();
 
-        var number = $('#registeredMOB').val();
-        var password = $('#password2').val();
+        //var number = $('#registeredMOB').val();
+        var password = $('#password3').val();
 
         var data1 = {
 
-            number: number,
+            //number: number,
             password: password
         };
 
         $.ajax(
             {
-                url: "/login",
+                url: "/updatepassword",
                 method: 'POST',
                 data: JSON.stringify(data1),
                 contentType: 'application/json',
