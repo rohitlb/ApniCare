66f7d81e123c8a407d99ad5ec6b2a6236c2c1dce
Rupesh <rupeshyadav94.ry@gmail.com>
2017-11-03 17:22:11 +0530

    changes

--- model/registration.js
+++ model/registration.js
@@ -3,8 +3,10 @@ var mongoose = require('mongoose');
 
 var User = new mongoose.Schema({
     Name : {type:String},
+    email : {tupe : String},
     Number : {type : String},
-    Password : {type:String}
+    Password : {type:String},
+    //profile : [{type : Schema.Types.ObjectId , ref : 'userprofile'} ]
 });
 
 module.exports = mongoose.model('user',User);
