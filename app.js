36c58918394e29cda13c1f0d9b42fb6b518fafb6
Rohit Singh <31705884+rohitlb@users.noreply.github.com>
2017-11-10 17:19:50 +0530

    -

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 // adding favicon of Apnicare
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, './', 'favicon.ico')));
 
 //set all middleware
 app.use(bodyParser.json());
