05d1ac41a876e019be5a50fcc408ce70ccb28fd7
Rohit Singh <31705884+rohitlb@users.noreply.github.com>
2018-01-14 14:36:52 +0530

    needhelp changes

--- model/needhelp.js
+++ model/needhelp.js
@@ -8,3 +8,18 @@ var Needhelp = new Schema({
 });
 
 module.exports = mongoose.model('needhelp',Needhelp);
+
+
+// there are 2 type
+//
+// 1 without registration
+// * need name, number, mail id...
+//     *about like= molecule data or any other kind of suggestion
+// usefullnes
+// suggestion
+
+// 2 with registration
+// * need name, number, mail id... from database // joining would decrease complexity and redundancy
+//     *about like= molecule data or any other kind of suggestion
+// usefullnes
+// suggestion 
