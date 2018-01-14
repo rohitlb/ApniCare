0f67ef4ccbbea1f49e439b6298869d5156e725ce
nidhi <32388884+Sonibrothers@users.noreply.github.com>
2018-01-14 18:18:08 +0530

    pharma

--- public/js/n_Ptest.js
+++ public/js/n_Ptest.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
 
         $.ajax(
             {
-                url: "/PharmacistsendOTP",
+                url: "/DoctorsendOTP",
                 method: 'POST',
                 data: JSON.stringify(data),
                 contentType: 'application/json',
@@ -42,7 +42,6 @@ $(document).ready(function() {
 
                 }
             }
-
         )
 
     });
@@ -77,7 +76,6 @@ $(document).ready(function() {
 
                 }
             }
-
         )
 
     });
@@ -101,7 +99,7 @@ $(document).ready(function() {
 
         $.ajax(
             {
-                url: "/pharmacistregister",
+                url: "/pharma",
                 method: 'POST',
                 data: JSON.stringify(data),
                 contentType: 'application/json',
@@ -123,171 +121,4 @@ $(document).ready(function() {
             }
         )
     });
-
-
-
-
-//for login
-/*
-    $('#PloginButton1').click(function () {
-
-        // $('#preloader').show();
-
-        var number = $('#number2').val();
-        var password = $('#password2').val();
-
-        var data1 = {
-
-            number: number,
-            password: password
-        };
-
-        $.ajax(
-            {
-                url: "/doctorlogin",
-                method: 'POST',
-                data: JSON.stringify(data1),
-                contentType: 'application/json',
-                success: function (result) {
-
-                    if (result.status === "success") {
-                        window.location = '/profiles';
-
-                    }
-                    else {
-                        Materialize.toast(result.message, 2000);
-                    }
-
-                },
-                error: function (err) {
-
-                    console.log(err);
-                }
-            }
-        )
-    });
-//for forgot password
-    $('#DOTPforForgot').click(function () {
-
-
-        // var name = $('#name').val();
-        var number = $('#registeredMOB').val();
-
-        var data = {
-            //name: name,
-            number: number
-        };
-
-        $.ajax(
-            {
-                url: "/doctorcheckforgotpassword",
-                method: 'POST',
-                data: JSON.stringify(data),
-                contentType: 'application/json',
-                success: function (result) {
-
-                    if (result.status === "success") {
-                        Materialize.toast(result.message, 2000);
-
-                        $('#OTPforForgot').hide();
-                        $('#pass1').show();
-                        $('#healthCare').hide();
-                        $('#Customer').hide();
-                        $('#password3').attr('disabled','disabled');
-                    }
-                    else {
-                        Materialize.toast(result.message, 2000);
-                        //$('#change').click(function () {
-                        //  $('#number').removeAttr('disabled');
-                        //$('#password').attr('disabled','disabled');
-
-                        //});
-                    }
-
-                },
-                error: function (err) {
-
-                }
-            }
-
-        )
-
-    });
-//for verification
-    $('#Dverify1').click(function () {
-        var otp = $('#otp1').val();
-
-        var data = {
-            number: otp
-        };
-
-        $.ajax(
-            {
-                url: "/VerifyOTP",
-                method: 'POST',
-                data: JSON.stringify(data),
-                contentType: 'application/json',
-                success: function (result) {
-
-                    if (result) {
-                        Materialize.toast(result.message, 2000);
-                        $('#password3').removeAttr('disabled');
-
-                    }
-                    else {
-                        Materialize.toast(result.message, 2000);
-
-                    }
-
-                },
-                error: function (err) {
-
-                }
-            }
-
-        )
-
-    });
-
-
-
-    $('#DagainLogin').click(function () {
-
-        //$('#preloader').show();
-
-        //var number = $('#registeredMOB').val();
-        var password = $('#password3').val();
-
-        var data1 = {
-
-            //number: number,
-            password: password
-        };
-
-        $.ajax(
-            {
-                url: "/doctorupdatepassword",
-                method: 'POST',
-                data: JSON.stringify(data1),
-                contentType: 'application/json',
-                success: function (result) {
-
-                    if (result.status === "success") {
-                        window.location = '/profiles';
-
-                    }
-                    else {
-                        Materialize.toast(result.message, 2000);
-                    }
-
-                },
-                error: function (err) {
-
-                    console.log(err);
-                }
-            }
-        )
-    });
-
-*/
 });
