864525c659d42ff40cafdbca1d7b3adc3e8eadf9
Rohit Singh <31705884+rohitlb@users.noreply.github.com>
2018-01-15 19:11:58 +0530

    -

--- model/needhelpWL.js
+++ model/needhelpWL.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var WLNeedhelp = new Schema({
+var NeedhelpWL = new Schema({
     name : {type : String},
     email : {type : String},
     number : {type : String},
