292530df5c0b532d8ca5bd31269c3e040c675d6c
Rohit Singh <31705884+rohitlb@users.noreply.github.com>
2018-01-17 19:04:32 +0530

    organ field added in disease schema

--- model/disease.js
+++ model/disease.js
@@ -4,10 +4,12 @@ var Schema = mongoose.Schema;
 
 var Disease = new Schema({
     disease_name : {type : String},
-    symptoms : {type:String},
+    symptoms : {type:String}, //a
     risk_factor : {type : String},
     cause : {type : String},
     diagnosis : [ {subhead1 : {type : String},subhead2 : {type : String}}],
+    // organ which are mainly affected
+    organ : [{subhead : {type : String},info : {type:String}}],
     treatment : {type : String},
     outlook : {type : String},
     prevention : {type : String},
