9cd8e09b0589920bc73ab0f34795d71e75cbf034
Rupesh yadav <rupeshyadav94.ry@gmail.com>
2018-01-20 16:27:48 +0530

    feedback

--- public/js/r_test.js
+++ public/js/r_test.js
@@ -826,7 +826,7 @@ $(function () {
     $('#submit_feedback').click(function () {
         var usefulness = $("input[type='radio'][name='radio1_feedback']:checked").val();
         var suggestion = $('#suggestion').val();
-        var about = $('#data_name').html();
+        var about = $('#about').text();
         alert(suggestion);
         var d = new Date();
         var month = d.getMonth()+1;
